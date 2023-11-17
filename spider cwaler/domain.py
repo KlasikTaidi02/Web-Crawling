3927d02b549acd33e42d9fa3004982b8c78d27bb
KlasikTaidi02 <taidiklasik@gmail.com>
2023-11-17 15:01:06 +0700

    first commit

---
+++ spider cwaler/domain.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse
+
+
+#Mendapt kan domain name (example.com)
+def get_domain_name(url):
+    try:
+        results = get_sub_domain_name(url).split('.')
+        return results[-2]+'.'+results[-1]
+    except:
+        return ''
+
+
+
+#Mendapt kan sub domain name (name.example.com)
+
+def get_sub_domain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
+    
+
